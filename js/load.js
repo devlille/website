db1430739897ac25e0569f96fd7b2b699bea18b6
gericci <contact@gericci.me>
2022-01-12 21:15:22 +0100

    first design sprint

--- js/load.js
+++ js/load.js
@@ -15,6 +15,32 @@ function ship() {
   });
 }
 
+// scroll event for header
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+function doSomething(scrollPos) {
+  let header = document.querySelector('#container > header');
+
+  if(scrollPos > 130) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+}
+
+document.addEventListener('scroll', function(e) {
+  lastKnownScrollPosition = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(lastKnownScrollPosition);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
 
 // Loading all scripts ...
 

eba98ec6f158787a0710c321d99ebc65717a4b90
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2022-02-10 21:00:23 +0100

    feat: start displaying sponsors

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,16 @@
 const htmlmin = require("html-minifier");
-const sponsors = require("./data/gold.json");
-
+const fetch = require("node-fetch")
 
 module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addCollection("partners", async () => {
+    const sponsors = await fetch("https://cms4partners-ce427.nw.r.appspot.com/events/vzbfowsExm54SrWLtxA5").then(res => res.json())
+    return sponsors.partners;
+  });
+
   eleventyConfig.addCollection("config", function () {
     return require("./data/config.json");
   });
-
-  eleventyConfig.addCollection("gold", function () {
-    return require("./data/gold.json");
-  });
-
 
   eleventyConfig.addCollection("speakers", function () {
     return require("./data/agenda.json").speakers;
@@ -19,35 +19,46 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("talks", function () {
     const config = require("./data/agenda.json")
     const talks = Object.entries(config.talks)
-    const oTalks = talks.map(([ _, talks]) => {
+    const oTalks = talks.map(([_, talks]) => {
       return [_, talks.map(talk => {
         return {
           ...talk,
-          speakers: talk.speakers?.map(speaker => config.speakers.find(({ uid }) => uid === speaker)?.displayName).join(', ')
+          speakers: talk.speakers?.map(speaker => config.speakers.find(({uid}) => uid === speaker)?.displayName).join(', ')
         }
       })]
     })
 
     return oTalks;
   });
-  eleventyConfig.addCollection("silver", function () {
-    return require("./data/silver.json");
-  });
-  eleventyConfig.addCollection("bronze", function () {
-    return require("./data/bronze.json");
-  });
-  eleventyConfig.addCollection("partners", function () {
-    return require("./data/partners.json");
-  });
-  eleventyConfig.setTemplateFormats(["png", "jpeg", "md", "html", "rss", "njk", "svg", "woff", "woff2"]);
 
-  eleventyConfig.addPassthroughCopy("css/**/*.*");
+
   eleventyConfig.addPassthroughCopy("sw.js");
+  eleventyConfig.addPassthroughCopy("css/*.ttf");
+  eleventyConfig.addPassthroughCopy("css/*.woff");
+  eleventyConfig.addPassthroughCopy("css/*.woff2");
   eleventyConfig.addPassthroughCopy("manifest.json");
-  eleventyConfig.addPassthroughCopy("*.pdf");
   eleventyConfig.addPassthroughCopy("js/*.*");
   eleventyConfig.addPassthroughCopy("img/*.*");
   eleventyConfig.addPassthroughCopy("partenaire.pdf");
+
+  eleventyConfig.setTemplateFormats(["md", "html", "rss", "njk"]);
+
+  eleventyConfig.addTemplateFormats("css");
+
+  const CleanCSS = require("clean-css");
+  eleventyConfig.addExtension("css", {
+    outputFileExtension: "css",
+    compile: async (inputContent) => {
+      return async () => {
+        return new Promise(resolve => {
+          new CleanCSS({ inline: ['remote'] }).minify(inputContent, (_, data) => {
+            resolve(data.styles)
+          });
+        });
+      };
+    }
+  });
+
 
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (outputPath.endsWith(".html")) {

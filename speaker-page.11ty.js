b4c273b3e855d71eb01ef685ba135e5fa62cbd0e
Emmanuel <demey.emmanuel@gmail.com>
2024-05-20 22:12:08 +0200

    feat: display all talks on the speaker page

--- speaker-page.11ty.js
+++ speaker-page.11ty.js
@@ -1,173 +1,65 @@
-const md = require("markdown").markdown;
+const { displaySpeakers, displaySpeakerSocialBlock } = require("./utils/speakers");
 
-const displaySpeakers = speakers => {
-    let result = "";
-    speakers.forEach(
-      (speaker) =>
-        (result += `<p>
-        <strong class="stressed">${speaker.display_name}${speaker.pronouns ? ` (${speaker.pronouns})` : ""}</strong>
-        ${speaker.company ? ", " : ""}
-        ${!!speaker.company ? speaker.company : ""} 
-    </p>
-    ${!!speaker.bio ? md.toHTML(speaker.bio.replaceAll("- ", "\r\n\r\n- ")) : ""}
-    `)
-    );
-    return result;
-}
 class SpeakerPage {
-    data() {
+  data() {
+    return {
+      layout: "layout.html",
+      currSection: "speakers",
+      pagination: {
+        data: "collections.speakersFromApi",
+        size: 1,
+        alias: "speaker",
+      },
+      permalink: (ctx) => {
+        return `speaker-page-${ctx.speaker.id}/`;
+      },
+      eleventyComputed: {
+        title: (ctx) => {
+          return ctx.speaker.display_name;
+        },
+        ogUrl: (ctx) => {
+          return "https://devfest.gdglille.org/speaker-page-" + ctx.speaker.id;
+        },
+        ogTitle: (ctx) => {
+          return ctx.speaker.display_name;
+        },
+        ogDescription: (ctx) => {
+          return ctx.speaker.bio?.replaceAll('"', "");
+        },
+        ogImage: (ctx) => {
+          return ctx.speaker.photo_url;
+        },
+      },
+    };
+  }
+  render(data) {
+    const speaker = data.collections.speakersFromApi.find((s) => s.id === data.speaker.id);
 
-        return {
-            layout: "layout.html",
-            currSection: "speakers",
-            pagination: {
-                data: "collections.speakersFromApi",
-                size: 1,
-                alias: 'speaker'
-            },
-            permalink: (ctx) => {
-                return `speaker-page-${ ctx.speaker.id }/`
-            },
-            eleventyComputed: {
-                title: (ctx) => {
-                    return ctx.speaker.display_name
-                },
-                ogUrl: (ctx) => {
-                    return "https://devfest.gdglille.org/speaker-page-" + ctx.speaker.id
-                },
-                ogTitle: (ctx) => {
-                    return ctx.speaker.display_name
-                },
-                ogDescription: (ctx) => {
-                    return ctx.speaker.bio?.replaceAll("\"", "")
-                },
-                ogImage: (ctx) => {
-                    return ctx.speaker.photo_url
-                },
+    let talks = Object.values(data.collections.talks)
+      .flat()
+      .map(([_, talks]) => talks ?? [])
+      .reduce((acc, talks) => [...acc, ...talks], []);
 
-            }
-        }
+    const selectedTalks = talks.filter((talk) => talk.speakersIds?.indexOf(data.speaker.id) >= 0);
+    if (selectedTalks.length === 0) {
+      return null;
     }
-    render(data){
-    
-        let talks = Object.values(data.collections.talks).flat().map(([_, talks]) => talks ?? []).reduce((acc, talks) => ([...acc, ...talks]), [])
 
-        const selectedTalk = talks.find(talk => talk.speakersIds?.indexOf(data.speaker.id) >= 0);
-        if(!selectedTalk){ 
-            return null;
-        }
-        const speakers = data.collections.speakersFromApi.filter(s => selectedTalk.speakersIds.indexOf(s.id) >= 0);
- 
-        const jsonld = {
-            "description": selectedTalk.talk.abstract,
-            "name": selectedTalk.talk.title,
-            "startDate": selectedTalk.talk.startTime,
-            "endDate": selectedTalk.talk.endTime,
-            "inLanguage": "fr",
-            "keywords": selectedTalk.talk?.talk?.category,
-            "location": selectedTalk.talk.room,
-            "maximumPhysicalAttendeeCapacity": selectedTalk.talk.room === 'Grand Théâtre' ? 1000 : 450,
-            "performer": {
-              "@type": "Person",
-              "image": speakers[0].photo_url,
-              "name": speakers[0].display_name,
-              "jobTitle": speakers[0].job_title ?? "",
-              "gender": speakers[0].pronouns ?? "",
-              "workFor": {
-                      "@type": "Organization",
-                      "name": speakers[0].company ?? ""
-                  }
-            }
-            
-          }
-        return `
-        <script type="application/ld+json">${JSON.stringify(jsonld)}</script>
-        <meta name="twitter:card" content="summary">
-        <meta name="twitter:site" content="@DevfestLille">
-        <meta name="twitter:creator" content="@DevfestLille">
-        <meta name="twitter:title" content="${selectedTalk.talk?.title?.replaceAll('"', "")}">
-        <meta name="twitter:description" content="${selectedTalk.talk?.abstract?.replaceAll('"', "")}">
-        <meta name="twitter:image" content="${speakers[0].photo_url}">
+    return `
 <div class="page-body list">
-    <h2>${selectedTalk.speakers}</h2>
+    <h2>${speaker.display_name}${speaker.pronouns ? ` (${speaker.pronouns})` : ""}</h2>
 
     <div class="speaker-sheet">
         <div class="speaker">
-            ${displaySpeakers(speakers)}
+            ${displaySpeakers(speaker)}
         </div>
 
-        <div class="speaker-data-block">
-            ${speakers.map(
-              (speaker) => `
-                <div class="speaker-data">
-
-                ${
-                  !!speaker.photo_url
-                    ? `<p class="s-photo">
-                      <img loading="lazy" src="${speaker.photo_url}" alt="Photo de ${speaker.display_name}" />
-                    </p>`
-                    : ""
-                }
-                <ul class="social">
-                    ${
-                      !!speaker.mastodon
-                        ? `<li>
-                        <strong class="stressed">Sur Mastodon</strong> 
-                        <a target="_blank" href="${speaker.mastodon}">${speaker.mastodon.substring(
-                            speaker.mastodon.lastIndexOf("/") + 1
-                          )}</a>
-                    </li>`
-                        : ""
-                    }
-                    ${
-                      !!speaker.twitter
-                        ? `<li>
-                        <strong class="stressed">Sur Twitter</strong>
-                        <a target="_blank" href="${speaker.twitter}">${speaker.twitter?.replace(
-                            "https://twitter.com/",
-                            ""
-                          )}</a>
-                    </li>`
-                        : ""
-                    }
-                    ${
-                      !!speaker.github
-                        ? `<li>
-                        <strong class="stressed">Sur Github</strong> 
-                        <a target="_blank" href="${speaker.github}">${speaker.github?.replace(
-                            "https://github.com/",
-                            ""
-                          )}</a>
-                    </li>`
-                        : ""
-                    }
-                    ${
-                      !!speaker.linkedin
-                        ? `<li>
-                        <strong class="stressed">Sur LinkedIn</strong> 
-                        <a target="_blank" href="${speaker.linkedin}">${decodeURI(speaker.linkedin)
-                            ?.replace("https://www.linkedin.com/in/", "")
-                            .replace("/", "")}</a>
-                    </li>`
-                        : ""
-                    }
-                    ${
-                      !!speaker.website
-                        ? `<li>
-                        <strong class="stressed">Site Web</strong> 
-                        <a target="_blank" href="${speaker.website}">${speaker.website}</a>
-                    </li>`
-                        : ""
-                    }
-                </ul>
-            </div>
-                `
-            )}
-            
-            
-        </div>
-
-        <div id="talk" class="talk">
+        ${displaySpeakerSocialBlock(speaker)}
+        <div class="talks">
+        ${selectedTalks
+          .map((selectedTalk) => {
+            return `
+                <div class="talk">
             <h3>${selectedTalk.talk?.title}</h3>
             <p>${selectedTalk.talk?.abstract}</p>
 
@@ -208,9 +100,13 @@ class SpeakerPage {
             }
             
         </div>
+                `;
+          })
+          .join("")}
+        </div>
     </div>
 </div>
 `;
-    }
+  }
 }
-module.exports = SpeakerPage
+module.exports = SpeakerPage;
---
+++ utils/speakers.js
@@ -0,0 +1,77 @@
+const md = require("markdown").markdown;
+
+exports.displaySpeakers = (speaker) => {
+  return `<p>
+  <strong class="stressed">${speaker.display_name}${speaker.pronouns ? ` (${speaker.pronouns})` : ""}</strong>
+  ${speaker.company ? ", " : ""}
+  ${!!speaker.company ? speaker.company : ""} 
+</p>
+${!!speaker.bio ? md.toHTML(speaker.bio.replaceAll("- ", "\r\n\r\n- ")) : ""}
+`;
+};
+
+exports.displaySpeakerSocialBlock = (speaker) => {
+  return `
+<div class="speaker-data-block">
+    <div class="speaker-data">
+    ${
+      !!speaker.photo_url
+        ? `<p class="s-photo">
+          <img loading="lazy" src="${speaker.photo_url}" alt="Photo de ${speaker.display_name}" />
+        </p>`
+        : ""
+    }
+    <ul class="social">
+        ${
+          !!speaker.mastodon
+            ? `<li>
+            <strong class="stressed">Sur Mastodon</strong> 
+            <a target="_blank" href="${speaker.mastodon}">${speaker.mastodon.substring(
+                speaker.mastodon.lastIndexOf("/") + 1
+              )}</a>
+        </li>`
+            : ""
+        }
+        ${
+          !!speaker.twitter
+            ? `<li>
+            <strong class="stressed">Sur Twitter</strong>
+            <a target="_blank" href="${speaker.twitter}">${speaker.twitter?.replace("https://twitter.com/", "")}</a>
+        </li>`
+            : ""
+        }
+        ${
+          !!speaker.github
+            ? `<li>
+            <strong class="stressed">Sur Github</strong> 
+            <a target="_blank" href="${speaker.github}">${speaker.github?.replace("https://github.com/", "")}</a>
+        </li>`
+            : ""
+        }
+        ${
+          !!speaker.linkedin
+            ? `<li>
+            <strong class="stressed">Sur LinkedIn</strong> 
+            <a target="_blank" href="${speaker.linkedin}">${decodeURI(speaker.linkedin)
+                ?.replace("https://www.linkedin.com/in/", "")
+                .replace("/", "")}</a>
+        </li>`
+            : ""
+        }
+        ${
+          !!speaker.website
+            ? `<li>
+            <strong class="stressed">Site Web</strong> 
+            <a target="_blank" href="${speaker.website}">${speaker.website}</a>
+        </li>`
+            : ""
+        }
+    </ul>
+</div>
+    
+
+</div>
+`;
+};
+
+exports.displayTalk = (talk) => {};

a8b043944a1dfc47697967c4896748eb394e3236
Emmanuel <demey.emmanuel@gmail.com>
2025-10-26 21:02:44 +0000

    feat. add youtube video

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,8 +2,8 @@ const config = {
   edition: 2026,
   editionNumber: 9,
   date: "11 et 12 Juin",
-  formattedStartDate: "2025-06-11",
-  formattedEndDate: "2025-06-12",
+  formattedStartDate: "2026-06-11",
+  formattedEndDate: "2026-06-12",
   billetwebUrl: "https://www.billetweb.fr/devlille-",
   cfpUrl: "https://conference-hall.io/devlille-",
   cmsUrl: "https://partenaire.devlille.fr/#",
@@ -12,6 +12,7 @@ const config = {
   cfpEndedDate: "31 Mars",
   partershipUrl:
     "https://docs.google.com/presentation/d/e/2PACX-1vSo0McftZg-uPKKsoJnKGmzny9pEB2G_6XHVPK3MXiEfMk2J4UgXGJ_JWS77namY9lw5Fqj1Ace8Ftr/pub?start=false&loop=false&delayms=3000",
+  youtubePlaylistId: "PLXbqr_rv1t4P8qX9IEgd3vnjm9IbEHDG8",
   numberOfPeople: 1500,
   numberOfSpeaker: 60,
   numberOfTalks: 44,
@@ -20,7 +21,6 @@ const config = {
     welovedevs: false,
     billetweb: false,
     sponsoring: false,
-    earlyBirds: false,
   },
 };
 
